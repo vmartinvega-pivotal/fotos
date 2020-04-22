032560947b50830bc2ccef69f172799fe426d1cb
Martin <vicente.vega@emc.com>
2020-04-22 13:20:38 +0200

    modified script

--- script.sh
+++ script.sh
@@ -85,9 +85,18 @@ do
 							INDEX_VIDEO_STRING="0$INDEX_VIDEO"
 						fi
 						
-						ls -1 -d "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER"/* | grep -E '.mp4|.MP4' > "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/list"
-						sed 's/^.*$/"&"/g' "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/list" > "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/list_mp4"
-						ffmpeg -f concat -i "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/list_mp4" -c copy "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/output"
+						VIDEO_AUX="0"
+						mkdir /tmp/mp4
+						for FILE_MP4 in `ls "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER" | grep -E '.mp4|.MP4'` # Grep mp4 or MP4
+                        do
+							VIDEO_AUX=$((VIDEO_AUX + 1))
+							cp "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/$FILE_MP4" "/tmp/mp4/$VIDEO_AUX.mp4"
+							echo "/tmp/mp4/$VIDEO_AUX.mp4" >> /tmp/list
+						done
+
+						ffmpeg -f concat -i /tmp/list -c copy "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/output"
+						rm -Rf /tmp/mp4
+						
 						#touch "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/output"
 						cp "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/output" "$PHOTOS_PATH/$YEAR-ori/$MONTH-$MONTH_HUMAN/$YEAR-$MONTH-$MONTH_HUMAN-VIDEO-$INDEX_VIDEO_STRING-$FOLDER.mp4"
 						rm "$PHOTOS_PATH/$YEAR/$MONTH/$FOLDER/list_mp4"
