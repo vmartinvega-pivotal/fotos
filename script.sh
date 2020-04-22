bca66f3a3e7068a7bdd38af981d28a219e04cc08
Martin <vicente.vega@emc.com>
2020-04-22 11:37:20 +0200

    modified script

--- script.sh
+++ script.sh
@@ -2,7 +2,19 @@
 
 PHOTOS_PATH=$1
 
-for FILE in `ls $PHOTOS_PATH`
+# Loop for years
+for YEAR in `ls $PHOTOS_PATH`
 do
-	echo $FILE
+	# Loop for months
+	for MONTH in `ls $PHOTOS_PATH/$YEAR`
+	do
+		# Loop for folders
+		for FOLDER in `ls $PHOTOS_PATH/$YEAR/$MONTH`
+		do
+			for FILE in `ls $PHOTOS_PATH/$YEAR/$MONTH/$FOLDER`
+			do
+				echo "File to analyze: $FILE"
+			done
+		done
+	done
 done
